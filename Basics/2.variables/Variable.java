18110c2beaf6f1b0448dcc355167327dbd3c0f54
atuldemond <atuldemond@gmail.com>
2024-11-03 17:49:19 +0530

    Just adding boilar plates

---
+++ Basics/2.variables/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String args[]){
+        int age = 23;
+        String name = "Atul Demond";
+        double height = 5.7;
+        System.out.println("My Name is"+" "+ name);
+        System.out.println("My age is "+" "+ age);
+        System.out.println("My height is"+" "+height);
+    }
+}
