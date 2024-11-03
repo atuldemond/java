8f8fd3bc9901f57d67feac00f6b3223371075a79
atuldemond <atuldemond@gmail.com>
2024-11-03 17:56:08 +0530

    boiler plates

--- Basics/2.variables/Variable.java
+++ Basics/2.variables/Variable.java
@@ -5,6 +5,11 @@ public class Variable {
         double height = 5.7;
         System.out.println("My Name is"+" "+ name);
         System.out.println("My age is "+" "+ age);
-        System.out.println("My height is"+" "+height);
+        System.out.println("My height is"+" "+height +"\n");
+        System.out.println("Just printing Stars\n \n");
+        System.out.println("*****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
