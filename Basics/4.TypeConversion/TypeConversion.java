015cf5798e507f9cd0a829c38ddb11be070b1334
atuldemond <atuldemond@gmail.com>
2024-11-05 15:49:53 +0530

    type conversion  is Java

---
+++ Basics/4.TypeConversion/TypeConversion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TypeConversion{
+    public static void main (String args[]){
+     /*
+      Type conversion , implicit conversion , wedining conversion 
+      
+      Rules:
+        3.compataible data type 
+        2.data type size small to large 
+     
+      */
+    }
+}
