2b294291f40d7c140d969d90883f369f77a8be93
atuldemond <atuldemond@gmail.com>
2024-11-05 16:28:12 +0530

    type conversion  is Java

---
+++ Basics/6.TypeConversion/TypeConversion.java
@@ -0,0 +1,12 @@
+import java.utils.*;
+public class TypeConversion{
+    public static void main (String args[]){
+        //1. type conversion means if byte short char => int
+        //=> (a+ b+ c) its treated as int 
+
+        //2. type conversion if long float double then => double
+        //=> (a+b+c ) its treated as double 
+
+        //** if any expression came into role then type conversion is Gone work forstly */
+    }
+}
