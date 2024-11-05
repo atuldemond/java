51bcf12e6cadb2c3bc8fb4088fab2da2aed38e43
atuldemond <atuldemond@gmail.com>
2024-11-05 14:35:57 +0530

    Data Type is Java

--- Basics/2.variables/Variable.java
+++ Basics/2.variables/Variable.java
@@ -1,15 +1,35 @@
 public class Variable {
     public static void main(String args[]){
-        int age = 23;
-        String name = "Atul Demond";
-        double height = 5.7;
-        System.out.println("My Name is"+" "+ name);
-        System.out.println("My age is "+" "+ age);
-        System.out.println("My height is"+" "+height +"\n");
+        // int age = 23;
+        // String name = "Atul Demond";
+        // double height = 5.7;
+        
+        
+        byte a = 2;
+        char ch = 'a';
+        int num = 23;
+        float fl= 23;
+        long lg= 23;
+        double db = 23;
+        boolean bl = true;
+        short sh = 23;
+        /*
+        theses is multi line comments 
+        */
+
+        // System.out.println("My Name is"+" "+ name);
+        // System.out.println("My age is "+" "+ age);
+        // System.out.println("My height is"+" "+height +"\n");
         System.out.println("Just printing Stars\n \n");
         System.out.println("*****");
         System.out.println("***");
         System.out.println("**");
         System.out.println("*");
+        
+
+        System.out.println("Adding Two Number ");
+        int num_1 = 45;
+        int num_2 = 56;
+        System.out.println("Sum of Two Number is "+(num_1+num_2));
     }
 }
