e6eb2e438835cb5a347559796530e35e6a2a7376
atuldemond <atuldemond@gmail.com>
2024-11-05 16:08:47 +0530

    type casting  is Java

---
+++ Basics/5.TypeCasting/TypeCasting.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class TypeCasting{
+    public static void main (String args[]){
+        //Type casting is processes of converting large data into small data types 
+        //in this process we will suffer from loss of data during conversion 
+
+        // names Typeconversion explicit narrawoing 
+
+        float f = 32.09f;
+        int a = (int )f;
+        System.out.println(a);
+
+    }
+}
