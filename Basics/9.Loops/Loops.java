090dee8759c223225846c3a876459b77509eeb9a
atuldemond <atuldemond@gmail.com>
2024-11-06 15:01:30 +0530

    loops statement  is Java

---
+++ Basics/9.Loops/Loops.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Loops{
+    public static void main(String args[]){
+            //While Loops 
+        //     int counter = 0;
+        //     while  (counter < 10) {
+        //     System.out.println("Hiii");
+        //     counter++;
+        //    }
+            //FOR LOOPS
+       // for(int i = 1 ; i<=10 ; i++){
+        //     System.out.println("Welcome to My World");
+        // }
+
+        for (int i = 0 ; i<4; i++) {
+            System.out.println("****");
+        }
+    }
+}
