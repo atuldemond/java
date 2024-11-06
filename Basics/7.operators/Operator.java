938b3334cf1fecc9445bed764651b56deaf6cb2d
atuldemond <atuldemond@gmail.com>
2024-11-06 13:51:33 +0530

    Operators  is Java

---
+++ Basics/7.operators/Operator.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Operator{
+    public static void main (Strings args[]){
+        //Arithmetic Operators
+        /***
+         * 1. BINARY OPERATOS
+         *      + - / * %
+         * 
+         * 2. UNARY OPERATOR
+         *      ++ --
+         *      
+         * 
+         *    1.  POST INCREMENT (a++) 2. pre INCREMENT (++aa)
+         *    2. Post Decremnt (a--) 2. pre Decrement (--a)
+         *    
+         * 
+         * RELATION OPERATORS
+         * ==
+         * !=
+         * <
+         * >
+         * >=
+         * <=
+         * 
+         * LOGICAL OPERATOR
+         * 
+         * &&
+         * ||
+         * !
+         * 
+         * ASSIGNEMNT OPERATORS
+         * =
+         * +=
+         * -=
+         * *=
+         * /=
+         * 
+         * 
+         */
+    }
+}
